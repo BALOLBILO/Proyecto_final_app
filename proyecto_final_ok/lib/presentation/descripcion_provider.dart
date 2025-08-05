10c9f2f75fcd9389a39fffef2208b33250df6b13
BALOLBILO <octavianosz@gmail.com>
2025-08-05 10:25:18 -0300

    a2

--- proyecto_final_ok/lib/presentation/descripcion_provider.dart
+++ proyecto_final_ok/lib/presentation/descripcion_provider.dart
@@ -1,4 +1,3 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:proyecto_final_ok/presentation/coordenadas_provider.dart';
 
 final descripcion = StateProvider<String>((ref) => '');
